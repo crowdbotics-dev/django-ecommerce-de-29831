385bf97b5276efb698a1eff8583a17386ca156b5
Crowdbotics <engineering@crowdbotics.com>
2022-08-21 17:14:40 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/django_ecommerce_de_29831/MainActivity.java
@@ -0,0 +1,15 @@
+package com.django_ecommerce_de_29831;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "django_ecommerce_de_29831";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "django_ecommerce_de_29831",
+  url: "https://django_ecommerce_de_29831.botics.co",
+  api: "https://django_ecommerce_de_29831.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
